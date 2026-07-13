1d5bd12c296f270fd9e05b00e367ccb045b27710
ecopraf <coppola.raffaele@gmail.com>
2026-07-13 16:10:03 +0200

    style: sidebar colore più chiaro (#1e3a5f) per migliore leggibilità

--- frontend-v2/src/build-info.js
+++ frontend-v2/src/build-info.js
@@ -2,12 +2,12 @@
 // SW Version: v3.16
 // Build Number: 50
 // Build ID: v3.16.50
-// Date: 13/07/2026, 09:05:31
+// Date: 13/07/2026, 16:09:12
 export const BUILD_INFO = {
   id: 'v3.16.50',
   version: 'v3.16',
   buildNumber: 50,
-  date: '2026-07-13T07:05:31.875Z',
-  buildDate: '13/07/2026, 09:05:31'
+  date: '2026-07-13T14:09:12.054Z',
+  buildDate: '13/07/2026, 16:09:12'
 };
 export const SW_VERSION = 'v3.16';
