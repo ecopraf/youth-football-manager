d7dee7715a1918814383b5e2ed72f3822436751c
ecopraf <coppola.raffaele@gmail.com>
2026-07-22 13:11:34 +0200

    feat: sposta bottone aggiornamenti nel dropdown avatar (sopra logout) [v3.16.93]

--- frontend-v2/src/build-info.js
+++ frontend-v2/src/build-info.js
@@ -1,13 +1,13 @@
 // Auto-generated build info
 // SW Version: v3.16
-// Build Number: 92
-// Build ID: v3.16.92
-// Date: 22/07/2026, 13:00:05
+// Build Number: 93
+// Build ID: v3.16.93
+// Date: 22/07/2026, 13:11:25
 export const BUILD_INFO = {
-  id: 'v3.16.92',
+  id: 'v3.16.93',
   version: 'v3.16',
-  buildNumber: 92,
-  date: '2026-07-22T11:00:05.541Z',
-  buildDate: '22/07/2026, 13:00:05'
+  buildNumber: 93,
+  date: '2026-07-22T11:11:25.083Z',
+  buildDate: '22/07/2026, 13:11:25'
 };
 export const SW_VERSION = 'v3.16';
